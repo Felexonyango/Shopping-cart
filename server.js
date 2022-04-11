2bc102c1098ab39e95d99b6a3388867879fe86f7
Felexonyango <felexonyango19@gmail.com>
2022-04-11 10:58:42 +0300

    hi

--- server.js
+++ server.js
@@ -17,15 +17,12 @@ app.use(cors());
 // app.use("/api/register", register);
 // app.use("/api/login", login);
 
-app.get("/", (req, res) => {
-  res.send("Welcome our to shop...");
-});
 
 app.get("/products", (req, res) => {
   res.send(products);
 });
 
-const uri = process.env.DB_URI;
+// const uri = process.env.DB_URI;
 const port = process.env.PORT || 5000;
 
 
